7b26cb885f8b69329271ba7ac3884d5eb9332070
Rupesh <rupeshmalpani03@gmail.com>
2024-04-28 15:12:22 +0530

    Changes in Login

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -12,118 +12,94 @@ import Context from "../context";
 
 
 const Login = () => {
-    const [showPassword,setShowPassword] = useState(false)
-    const [data,setData] = useState({
-        email : "",
-        password : ""
+    const [showPassword, setShowPassword] = useState(false);
+    const [data, setData] = useState({
+        email: "",
+        password: ""
     })
-    const navigate = useNavigate()
-    const { fetchUserDetails, fetchUserAddToCart } = useContext(Context)
+    const navigate=useNavigate();
+    const {fetchUserDetails}=useContext(Context)
+   
 
-    const handleOnChange = (e) =>{
-        const { name , value } = e.target
-
-        setData((preve)=>{
-            return{
-                ...preve,
-                [name] : value
-            }
-        })
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setData({ ...data, [name]: value })
     }
 
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
 
-    const handleSubmit = async(e) =>{
-        e.preventDefault()
-
-        const dataResponse = await fetch(SummaryApi.signIn.url,{
-            method : SummaryApi.signIn.method,
-            credentials : 'include',
-            headers : {
-                "content-type" : "application/json"
+        const dataResponse=await fetch(SummaryApi.signIn.url,{
+            method:SummaryApi.signIn.method,
+            credentials:'include',
+            headers:{
+                "Content-Type":"application/json"
             },
-            body : JSON.stringify(data)
+            body:JSON.stringify(data)
         })
 
-        const dataApi = await dataResponse.json()
+        const dataApi=await dataResponse.json();
 
         if(dataApi.success){
             toast.success(dataApi.message)
-            navigate('/')
+            navigate('/');
             fetchUserDetails()
-            fetchUserAddToCart()
+            
         }
 
         if(dataApi.error){
             toast.error(dataApi.message)
         }
-
     }
 
-    console.log("data login",data)
-    
-  return (
-    <section id='login'>
-        <div className='mx-auto container p-4'>
-
-            <div className='bg-white p-5 w-full max-w-sm mx-auto'>
-                    <div className='w-20 h-20 mx-auto'>
-                        <img src={loginIcons} alt='login icons'/>
+    return (
+        <section id="login">
+            <div className="mx-auto container p-4">
+                <div className="bg-white p-5 w-full max-w-sm mx-auto">
+                    <div className="w-20 h-20 mx-auto">
+                        <img src={loginIcons} alt="login icons" />
                     </div>
-
-                    <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
-                        <div className='grid'>
-                            <label>Email : </label>
-                            <div className='bg-slate-100 p-2'>
-                                <input 
-                                    type='email' 
-                                    placeholder='enter email' 
-                                    name='email'
+                    <form className="pt-6 flex flex-col gap-2" onSubmit={handleSubmit}>
+                        <div className="grid">
+                            <label>Email :</label>
+                            <div className="bg-slate-100 p-2">
+                                <input
+                                    type="email"
+                                    placeholder="enter your email"
                                     value={data.email}
-                                    onChange={handleOnChange}
-                                    className='w-full h-full outline-none bg-transparent'/>
+                                    onChange={handleChange}
+                                    autoComplete="off"
+                                    required
+                                    name="email"
+                                    className="w-full h-full outline-none bg-transparent"
+                                />
                             </div>
                         </div>
-
                         <div>
-                            <label>Password : </label>
-                            <div className='bg-slate-100 p-2 flex'>
-                                <input 
-                                    type={showPassword ? "text" : "password"} 
-                                    placeholder='enter password'
+                            <label>Password:</label>
+                            <div className="bg-slate-100 p-2 flex">
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    placeholder="enter your password"
                                     value={data.password}
-                                    name='password' 
-                                    onChange={handleOnChange}
-                                    className='w-full h-full outline-none bg-transparent'/>
-                                <div className='cursor-pointer text-xl' onClick={()=>setShowPassword((preve)=>!preve)}>
-                                    <span>
-                                        {
-                                            showPassword ? (
-                                                <FaEyeSlash/>
-                                            )
-                                            :
-                                            (
-                                                <FaEye/>
-                                            )
-                                        }
-                                    </span>
+                                    onChange={handleChange}
+                                    required
+                                    name="password"
+                                    className="w-full h-full outline-none bg-transparent"
+                                />
+                                <div className="cursor-pointer text-xl" onClick={() => setShowPassword((preve) => !preve)}>
+                                    <span>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
                                 </div>
                             </div>
-                            <Link to={'/forgot-password'} className='block w-fit ml-auto hover:underline hover:text-red-600'>
-                                Forgot password ?
-                            </Link>
+                            <Link to={'/forgot-password'} className='block w-fit ml-auto hover:underline hover:text-red-500'>Forgot Password?</Link>
                         </div>
-
-                        <button className='bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>Login</button>
-
+                        <button className="bg-red-600 text-white px-6 py-2 w-full max-w-[150px] hover:bg-red-700 rounded-full hover:scale-110 transition-all mx-auto block mt-4">Login</button>
                     </form>
-
-                    <p className='my-5'>Don't have account ? <Link to={"/sign-up"} className=' text-red-600 hover:text-red-700 hover:underline'>Sign up</Link></p>
+                    <p className="py-5">Don't have Account? <Link to={'/sign-up'} className="text-red-600 hover:text-red-700 hover:underline">Sign up</Link></p>
+                </div>
             </div>
+        </section>
+    );
+};
 
-
-        </div>
-    </section>
-  )
-}
-
-export default Login
+export default Login;
